81ce63fe08dae25c71449a7ff201ab0d7e3f360c
rifqiraehan <rifqiraehan86@gmail.com>
2023-01-20 05:45:36 +0700

    initial

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,6 +19,36 @@ class UserSeeder extends Seeder
             'role_id' => '1',
         ]);
 
+        User::create([
+            'name' => 'Bu Jane',
+            'email' => 'penjual@gmail.com',
+            'username' => 'penjual',
+            'lokasi' => 'Lokasi A',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'role_id' => '2',
+        ]);
+
+        User::create([
+            'name' => 'Pak John',
+            'email' => 'partner@gmail.com',
+            'username' => 'partner',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'role_id' => '3',
+        ]);
+
+        User::create([
+            'name' => 'Budi Santoso',
+            'email' => 'murid@gmail.com',
+            'username' => 'murid',
+            'kelas' => 'X',
+            'jurusan' => 'RPL 1',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'role_id' => '4',
+        ]);
+
         User::factory(5)->penjual()->create();
 
         User::factory(5)->kurir()->create();
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard') }}  {{ Auth::user()->role->name }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-xl">
             <h1>Welcome,<strong> {{ Auth::user()->username }}</strong></h1>
             <h1>Jumlah pengguna terdaftar pada <em>Canteen Co.</em> adalah {{ Auth::user()->count() }}</h1>
-            {{-- <h1>Anda adalah <strong>{{ Auth::user()->role->name }}</strong></h1> --}}
         </div>
     </div>
 </x-app-layout>
