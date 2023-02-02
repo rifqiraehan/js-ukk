36166630cca840aa48f3deb12337f33e0c182162
rifqiraehan <rifqiraehan86@gmail.com>
2023-02-02 09:28:35 +0700

    change a lang-err

---
+++ lang/id/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |---------------------------------------------------------------------------------------
+    | Baris Bahasa untuk Pengingat Kata Sandi
+    |---------------------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris standar yang cocok dengan alasan yang
+    | diberikan oleh pembongkar kata sandi yang telah gagal dalam upaya pembaruan
+    | kata sandi, misalnya token tidak valid atau kata sandi baru tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah direset!',
+    'sent' => 'Kami sudah mengirim tautan reset kata sandi ke email Anda!',
+    'throttled' => 'Silakan tunggu sebelum mencoba lagi.',
+    'token' => 'Token reset kata sandi ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+
+];
