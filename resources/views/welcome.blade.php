a003611026873f14a210eccf2e1106419211bb63
rifqiraehan <rifqiraehan86@gmail.com>
2023-02-04 13:27:19 +0700

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a quasi obcaecati labore incidunt! Blanditiis, omnis! Excepturi, facilis consectetur. Amet, tempore? Delectus aspernatur velit corporis iusto nobis. Amet ut nihil ex ratione possimus. Doloribus, ex tenetur, dolorum, aut deserunt repudiandae id veritatis beatae accusamus quos perspiciatis iusto blanditiis culpa nostrum.
                                 </div>
                             </div>
                         </div>
@@ -68,7 +68,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellendus repudiandae eius alias error id maiores ab excepturi assumenda rerum quo dolorum ex nesciunt est at quis, accusamus quod fugiat asperiores quasi, pariatur, corrupti hic. Molestias perferendis ipsam maxime assumenda!
                                 </div>
                             </div>
                         </div>
@@ -81,7 +81,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel optio autem nostrum totam culpa nihil, ut minus, id molestiae quod maxime alias dolorem perspiciatis tempora dolor doloribus est temporibus.
                                 </div>
                             </div>
                         </div>
@@ -89,12 +89,12 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</div>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Tentang</div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, sapiente sint? Dolore, quod. Possimus, obcaecati vitae! Doloremque provident corrupti repellat perspiciatis consequuntur a, magni doloribus praesentium, dolores aut, similique reprehenderit facere quas culpa optio pariatur eligendi cumque tempora perferendis quos ipsam quidem voluptatem. Reprehenderit, autem. Porro accusantium nam praesentium perspiciatis?
                                 </div>
                             </div>
                         </div>
