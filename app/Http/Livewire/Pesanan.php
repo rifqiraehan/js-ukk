5cd6f54191232be2e355cfe91328e2bb3e9735ff
rifqiraehan <rifqiraehan86@gmail.com>
2023-02-22 05:41:14 +0700

    prepare for filter and search pesanan

--- app/Http/Livewire/Pesanan.php
+++ app/Http/Livewire/Pesanan.php
@@ -4,33 +4,46 @@ namespace App\Http\Livewire;
 
 use App\Models\Order;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Pesanan extends Component
 {
+    use WithPagination;
+
     protected $orders;
-    public $status;
+    public $search;
+    public $statusFilter;
+
+    public $searchTerm = null;
+    protected $queryString = ['searchTerm' => ['except' => '']];
 
     public function mount()
     {
         $user = auth()->user();
 
-        $this->status = 'all';
-
         $this->orders = Order::whereHas('orderItems.product', function ($query) use ($user) {
-            $query->where('user_id', $user->id);
-        })
-        ->with('user')
-        ->orderByDesc('created_at')
-        ->paginate(5);
+                $query->where('user_id', $user->id);
+            })
+            ->with('user')
+            ->when($this->search, function ($query) {
+                $query->where('status', 'like', '%' . $this->search . '%')
+                      ->orWhere('name', 'like', '%' . $this->search . '%');
+            })
+            ->when($this->statusFilter, function ($query) {
+                $query->where('status', $this->statusFilter);
+            })
+            ->orderByDesc('created_at')
+            ->paginate(5);
     }
 
-    public function updateStatus($orderId, $status)
+    public function updatingSearch()
     {
-        $order = Order::find($orderId);
-        $order->orderStatus->status = $status;
-        $order->orderStatus->save();
+        $this->resetPage();
+    }
 
-        $this->mount();
+    public function updatingStatusFilter()
+    {
+        $this->resetPage();
     }
 
     public function render()
@@ -40,3 +53,4 @@ class Pesanan extends Component
         ]);
     }
 }
+
--- resources/views/livewire/pesanan.blade.php
+++ resources/views/livewire/pesanan.blade.php
@@ -1,5 +1,44 @@
 <div>
     <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="mb-4 flex justify-between items-center">
+            <div class="flex-1 pr-4"></div>
+            {{-- <button id="generate-pdf" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Print</button> --}}
+
+            <!-- Filter By Status -->
+                <div class="relative text-gray-700 focus-within:text-gray-600 m-2">
+                    <select wire:model="statusFilter" id="statusFilter" class="py-2 text-sm text-gray bg-gray-100 rounded-md focus:outline-none focus:bg-gray-200 focus:text-gray-700">
+                        <option value="">All</option>
+                        <option value="Menunggu Konfirmasi">Belum Dikonfirmasi</option>
+                        <option value="Pesanan Dikonfirmasi">Pesanan Dikonfirmasi</option>
+                        <option value="Pesanan Siap">Pesanan Siap</option>
+                        <option value="Pesanan Selesai">Pesanan Selesai</option>
+                        <option value="Pesanan Dibatalkan">Pesanan Dibatalkan</option>
+                    </select>
+                </div>
+
+
+                <div>
+                    <!-- Searching -->
+                    <div class="relative text-gray-700 focus-within:text-gray-600">
+                        <div class="relative md:w-1/3">
+                            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                                <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
+                                    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                    </svg>
+                                </button>
+                            </span>
+                            <input wire:model="search" type="search"
+                            class="py-2 text-sm text-gray
+                            bg-gray-100 rounded-md pl-10 focus:outline-none
+                            focus:bg-gray-200 focus:text-gray-700"
+                            placeholder="{{ __('Search...') }}"
+                            autocomplete="off">
+                        </div>
+                    </div>
+                </div>
+        </div>
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -12,23 +51,23 @@
                                         #
                                     </th>
                                     <th scope="col"
-                                        class="py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        class="px-2 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Tanggal Pesan
                                     </th>
                                     <th scope="col"
-                                        class="py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        class="px-2 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Pembeli
                                     </th>
                                     <th scope="col" width="200"
-                                        class="py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        class="px-2 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Total
                                     </th>
                                     <th scope="col" width="200"
-                                        class="px-1 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        class="px-2 px-1 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Status
                                     </th>
                                     <th scope="col" width="200"
-                                        class="px-1 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        class="px-2 px-1 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Aksi
                                     </th>
                                 </tr>
@@ -40,12 +79,12 @@
                                             {{ $loop->iteration + $orders->firstItem() - 1 }}
                                         </td>
 
-                                        <td class="py-4 whitespace-nowrap text-sm text-gray-900">
+                                        <td class="px-2 py-4 whitespace-nowrap text-sm text-gray-900">
                                             {{ $order->created_at->format('j M Y') }},
                                             {{ $order->created_at->format('H:i') }} WIB
                                         </td>
 
-                                        <td class="py-4 whitespace-nowrap text-sm text-gray-900">
+                                        <td class="px-2 py-4 whitespace-nowrap text-sm text-gray-900">
                                             {{ $order->user->name }} <br>
                                             <span class="text-gray-500">{{ $order->user->kelas }}
                                                 {{ $order->user->jurusan }}</span>
@@ -57,7 +96,7 @@
                                             @endforeach
                                         </td> --}}
 
-                                        <td class="py-4 whitespace-nowrap text-sm text-gray-900">
+                                        <td class="px-2 py-4 whitespace-nowrap text-sm text-gray-900">
                                             Rp {{ number_format($order->total, 0, '.', '.') }}
                                         </td>
 
